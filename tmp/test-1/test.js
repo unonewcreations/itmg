74b6c474a60b4eb66ef286728ff9de13b9a627fe
unonewcreative <unonewcreative@gmail.com>
2021-11-18 19:19:41 +0100

    update

---
+++ tmp/test-1/test.js
@@ -0,0 +1,4 @@
+let element = document.getElementById("demo");
+
+element.innerHTML = "Hello World!";
+
---
+++ tmp/test-2/test.js
@@ -0,0 +1,6 @@
+function multiply() {
+    let result = 20 *20;
+    console.log(result);
+}
+
+multiply();
