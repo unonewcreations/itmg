a8580b1108270082f276e83f4a85d62cc4ba9ce3
unonewcreative <unonewcreative@gmail.com>
2021-11-18 13:04:37 +0100

    update

--- Part-1/HTMLCSSJSExercises/Student Files/18JavaScriptBasics/Demos/script-2.js
+++ Part-1/HTMLCSSJSExercises/Student Files/18JavaScriptBasics/Demos/script-2.js
@@ -4,3 +4,18 @@ This script simply outputs
 to the browser.
 */
 document.write("<p>Hello, there!</p>");
+
+// oefening manier 1
+document.write("<div id='test'>test</div>");
+let test = document.getElementById('test');
+console.log(test);
+test.addEventListener("click", function() {
+	document.write("geklikt op de tekst");
+});
+
+// oefening manier 2
+function functionA () {
+	document.write("geklikt op de tekst");
+}
+
+functionA();
